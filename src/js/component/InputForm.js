5ca98d5ed1840c15c8f2ce2bdb21e208f6f17726
Herick1994 <hericksalcedo1994@gmail.com>
2019-11-21 18:32:53 -0400

    boilerplate listo

---
+++ src/js/component/InputForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const InputForm = props => {
+	return (
+		<nav className="navbar navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				Navbar
+			</a>
+		</nav>
+	);
+};
+
+export default InputForm;
---
+++ src/js/component/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import InputForm from "./InputForm.js";
+
+//create your first component
+export function Home() {
+	return <InputForm />;
+}
